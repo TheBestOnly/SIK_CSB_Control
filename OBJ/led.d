1387ec10b9e12d12166294733eb3dfdacb356f8d
TheBestOnly <624589971@qq.com>
2016-01-06 16:02:50 +0800

    Revert "add control"
    This reverts commit b7e42a771e06c256e768dd4cad872e9b7f5a54bd.

--- OBJ/led.d
+++
@@ -1,9 +0,0 @@
-..\obj\led.o: ..\HEADWARE\led.c
-..\obj\led.o: ..\HEADWARE\led.h
-..\obj\led.o: ..\SYSTEM\sys\sys.h
-..\obj\led.o: ..\SYSTEM\sys\stm32f10x.h
-..\obj\led.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.1.1\CMSIS\Include\core_cm3.h
-..\obj\led.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\stdint.h
-..\obj\led.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.1.1\CMSIS\Include\core_cmInstr.h
-..\obj\led.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.1.1\CMSIS\Include\core_cmFunc.h
-..\obj\led.o: ..\SYSTEM\sys\system_stm32f10x.h
